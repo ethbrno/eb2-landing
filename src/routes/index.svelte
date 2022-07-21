055917e5b31dea648674f5f253e3410d7455e930
tree <tree@hidden.cz>
2022-07-21 20:24:57 +0200

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,16 +4,18 @@
 
 
 <div class="flex justify-center min-h-screen items-center bg-[#15202B] text-white transition-all pt-2 pb-2">
-  <div class="text-center px-10 lg:px-4 lg:w-1/2 mt-4 mb-4">
+  <div class="text-center px-10 lg:px-4 lg:w-1/2 mt-4 mb-10">
     <div class="text-center mb-8"><img src="/logo-red.png" class="inline-block w-[300px]" /></div>
     <div class="transition-all text-[#E73845] "><h1 class="text-6xl opacity-100">ETHBrno²<br/><span class="whitespace-nowrap">(🔑,😎)</span></h1></div>
-    <div class="mt-8 text-xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
+    <div class="mt-8 text-2xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
     <div class="mt-4 text-sm opacity-50 lowercase">#anonymity #non-kyc #encryption #selfsovereignty #p2p #smartcontracts #audits #vulnerabilities #analytics #wallets #scams #MEV #zeroknowledgeproofs #zkp </div>
-    <div class="mt-8">October 2022 @ Brno, Czech Republic</div>
+    <div class="mt-4 text-xl uppercase">October 2022 @ Brno, Czech Republic</div>
+    <!--div class="mt-8 text-sm">Apply to <a href="#" class="underline hover:text-[#E73845] transition-all">hack</a>, <a href="#" class="underline hover:text-[#E73845] transition-all">speak</a>, <a href="#" class="underline hover:text-[#E73845] transition-all">volunteer</a> ― <a href="#" class="underline hover:text-[#E73845] transition-all">Sponsor us</a></div-->
     <div class="mt-8 text-3xl transition-all opacity-80">
-      <a href="https://twitter.com/ethbrno" target="_blank"><i class="fab fa-twitter hover:text-[#E73845] transition-all"></i></a>
       <a href="https://docs.ethbrno.cz/2022" target="_blank"><i class="fa-solid fa-book hover:text-[#E73845] transition-all"></i></a>
       <a href="https://github.com/gweicz/ethbrno/" target="_blank"><i class="fa-brands fa-github hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://matrix.to/#/#ethbrno:matrix.org" target="_blank"><i class="fa-solid fa-comment hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://twitter.com/ethbrno" target="_blank"><i class="fab fa-twitter hover:text-[#E73845] transition-all"></i></a>
     </div>
   </div>
 </div>
