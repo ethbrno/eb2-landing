7d32b3c7c201b046eb03d01aa7ba5c15b6640754
tree <tree@hidden.cz>
2022-07-21 19:18:29 +0200

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,9 +5,9 @@
 
 <div class="flex justify-center min-h-screen items-center bg-[#15202B] text-white transition-all pt-2 pb-2">
   <div class="text-center px-10 lg:px-4 lg:w-1/2 mt-4 mb-4">
-    <div class="text-center mb-8"><img src="/logo-red.png" class="inline-block w-[350px]" /></div>
-    <div class="transition-all text-[#E73845] "><h1 class="text-7xl opacity-100">ETHBrno²</h1></div>
-    <div class="mt-8 text-2xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
+    <div class="text-center mb-8"><img src="/logo-red.png" class="inline-block w-[300px]" /></div>
+    <div class="transition-all text-[#E73845] "><h1 class="text-6xl opacity-100">ETHBrno²<br/><span class="whitespace-nowrap">(🔑,😎)</span></h1></div>
+    <div class="mt-8 text-xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
     <div class="mt-4 text-sm opacity-50 lowercase">#anonymity #non-kyc #encryption #selfsovereignty #p2p #smartcontracts #audits #vulnerabilities #analytics #wallets #scams #MEV #zeroknowledgeproofs #zkp </div>
     <div class="mt-8">October 2022 @ Brno, Czech Republic</div>
     <div class="mt-8 text-3xl transition-all opacity-80">
