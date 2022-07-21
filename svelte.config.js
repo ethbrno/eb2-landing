9a1b7f4e2b59c43b5b74624f6258add3e1d8d99c
tree <tree@hidden.cz>
2022-07-21 16:56:47 +0200

    Update

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,11 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+    prerender: {
+      default: true
+    }
 	}
 };
 
