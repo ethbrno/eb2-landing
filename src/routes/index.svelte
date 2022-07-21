dbbb1dd947391adf9212052507c5c8107800020f
tree <tree@hidden.cz>
2022-07-21 19:03:21 +0200

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 
 <div class="flex justify-center items-center h-screen bg-[#15202B] text-white transition-all">
-  <div class="text-center px-10 lg:px-4 lg:w-1/2">
+  <div class="text-center px-10 lg:px-4 lg:w-1/2 pt-2">
     <div class="text-center mb-8"><img src="/logo-red.png" class="inline-block w-[350px]" /></div>
     <div class="transition-all text-[#E73845] "><h1 class="text-7xl opacity-100">ETHBrno²</h1></div>
     <div class="mt-8 text-2xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
