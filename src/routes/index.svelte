38b29794a9e7be45aae1e320279467ffd08ba6df
tree <tree@hidden.cz>
2022-07-21 18:31:50 +0200

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>ETHBrno²</title>
+</svelte:head>
+
+
+<div class="flex justify-center items-center h-screen bg-[#15202B] text-white transition-all">
+  <div class="text-center px-10 lg:px-4 lg:w-1/2">
+    <div class="text-center mb-8"><img src="/logo-red.png" class="inline-block w-[350px]" /></div>
+    <div class="transition-all text-[#E73845] "><h1 class="text-7xl opacity-100">ETHBrno²</h1></div>
+    <div class="mt-8 text-2xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
+    <div class="mt-4 text-sm opacity-50 lowercase">#anonymity #non-kyc #encryption #decentralization #p2p #smartcontracts #audits #vulnerabilities #analytics #wallets #scams #MEV #zeroknowledgeproofs #zkp </div>
+    <div class="mt-8">October 2022 @ Brno, Czech Republic</div>
+    <div class="mt-8 text-3xl transition-all opacity-80">
+      <a href="https://twitter.com/ethbrno" target="_blank"><i class="fab fa-twitter hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://docs.ethbrno.cz/" target="_blank"><i class="fa-solid fa-book hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://github.com/gweicz/ethbrno/" target="_blank"><i class="fa-brands fa-github hover:text-[#E73845] transition-all"></i></a>
+    </div>
+  </div>
+</div>
