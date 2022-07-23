4e271a8f413b3e3ce11a8295824f756ba43a4ff5
tree <tree@hidden.cz>
2022-07-23 09:03:44 +0200

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex justify-center min-h-screen items-center bg-[#15202B] text-white transition-all pt-2 pb-2">
   <div class="text-center px-10 lg:px-4 lg:w-1/2 mt-4 mb-10">
-    <div class="text-center mb-8"><a href="https://docs.ethbrno.cz/events/2022"><img src="/logo-red.png" class="inline-block w-[300px] animate-pulse hover:animate-none hover:w-[400px] transition-all" /></a></div>
+    <div class="text-center mb-8"><a href="https://docs.ethbrno.cz/events/2022"><img src="/logo-red.png" class="inline-block w-[300px] animate-pulse hover:animate-none transition-all" /></a></div>
     <div class="transition-all text-[#E73845] "><h1 class="text-6xl opacity-100">ETHBrno²<br/><span class="whitespace-nowrap">(🔑,😎)</span></h1></div>
     <div class="mt-8 text-2xl uppercase tracking-widest font-italic">&gt; Privacy & Security edition &lt;</div>
     <div class="mt-4 text-sm opacity-50 lowercase">#freedom #anonymity #non-kyc #encryption #selfsovereignty #p2p #opensource #web3 #smartcontracts #audits #vulnerabilities #analytics #wallets #scams #darkforest #MEV #zeroknowledgeproofs</div>
