98b43a4ec452df40333a2c78da9b9a45596b012b
tree <tree@hidden.cz>
2022-08-23 18:12:45 +0200

    Add hacker application form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,10 +11,15 @@
     <div class="mt-4 text-sm opacity-50 lowercase">#freedom #anonymity #non-kyc #encryption #selfsovereignty #p2p #opensource #web3 #smartcontracts #audits #vulnerabilities #analytics #wallets #scams #darkforest #MEV #zeroknowledgeproofs</div>
     <div class="mt-4 text-xl uppercase">November 11-13, 2022 @ Brno, Czech Republic</div>
     <!--div class="mt-8 text-sm">Apply to <a href="#" class="underline hover:text-[#E73845] transition-all">hack</a>, <a href="#" class="underline hover:text-[#E73845] transition-all">speak</a>, <a href="#" class="underline hover:text-[#E73845] transition-all">volunteer</a> ― <a href="#" class="underline hover:text-[#E73845] transition-all">Sponsor us</a></div-->
+    <div class="mt-4 text-sm">For more informations read <a href="https://mirror.xyz/ethbrno.eth/6BH9cUVuD85hy5O0L5cOOOE7niSA9Yo5eWsXVzKOlO4" target="_blank" class="underline hover:no-underline">Announcing ETHBrno²</a> article or visit <a href="https://docs.ethbrno.cz/events/2022" target="_blank" class="underline hover:no-underline">our documentation</a></div>
+    <div class="mt-8">
+      <a href="https://cryptpad.fr/form/#/2/form/view/JFQ+WOryuHqPJAna0BOoLka3Lh+JPGrPSm4SWWFYYx0/" class="py-2 px-4 border border-[#E73845]/50 bg-[#E73845] rounded uppercase hover:bg-[#E73845]/50 text-xl transition-all">Apply to hack</a></div>
     <div class="mt-8 text-3xl transition-all opacity-80">
       <a href="https://docs.ethbrno.cz/events/2022" target="_blank"><i class="fa-solid fa-book hover:text-[#E73845] transition-all"></i></a>
       <a href="https://github.com/gweicz/ethbrno/" target="_blank"><i class="fa-brands fa-github hover:text-[#E73845] transition-all"></i></a>
       <a href="https://matrix.to/#/#ethbrno:matrix.org" target="_blank"><i class="fa-solid fa-comment hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://discord.com/invite/qTCka7qtPZ" target="_blank"><i class="fa-brands fa-discord hover:text-[#E73845] transition-all"></i></a>
+      <a href="https://mirror.xyz/ethbrno.eth" target="_blank"><i class="fa-solid fa-newspaper hover:text-[#E73845] transition-all"></i></a>
       <a href="https://twitter.com/ethbrno" target="_blank"><i class="fab fa-twitter hover:text-[#E73845] transition-all"></i></a>
     </div>
   </div>
